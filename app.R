bb1f088299d4b4f222552c250b469d69068ca72a
Sarah Bonnin <sbonnin@W89306F766J.local>
2018-11-20 17:40:55 +0100

    finalizing metadata table input

--- app.R
+++ app.R
@@ -17,20 +17,20 @@ options(shiny.maxRequestSize=100*1024^2) # sets the max file to load to 100Mb
 ui <- fluidPage(
   
   # Application title
-  titlePanel("PCA plot"),
+  titlePanel("Principal Component Analysis (PCA)"),
   
   # Sidebar with options
   sidebarLayout(
     sidebarPanel(
-      # input file: accepts text and csv files
-      fileInput("file_pca", "Upload expression matrix", accept = c("text/txt", "text/csv")),
+      # input file: accepts tab-separated text files
+      fileInput("file_pca", "Upload expression matrix", accept = c("text/txt", "text/csv", "text/tab")),
       # input file: sample table that will tell what groups samples belong to
-      fluidRow(column(9, fileInput("sample_table", "Upload sample table file", accept = c("text/txt", "text/csv"))),
+      fluidRow(column(9, fileInput("sample_table", "(optional) Upload sample metadata file", accept = c("text/txt", "text/csv", "text/tab"))),
                column(3, bsButton("q1", label = "", icon = icon("question"), 
                                   style = "success", size = "extra-small", block=T))),
       # text that appears when the user clicks on the "bsButton" question mark
-      bsPopover(id = "q1", title = "sample table",
-                content = paste0("file that contains columns with samples grouping and other characteristics. The first column must correspond to the column names of the input file!"),
+      bsPopover(id = "q1", title = "Metadata file:",
+                content = paste0("contains columns with samples metadata: the first column must correspond to the first row of the input file!"),
                 placement = "right", 
                 trigger = "click", 
                 options = list(container = "body")),
@@ -40,24 +40,29 @@ ui <- fluidPage(
       # also the potential grouping and naming columns from the sample table appear
       uiOutput("sample_grouping_factor"),
       uiOutput("sample_point_labels"),
+      uiOutput("sample_point_shapes"),
       #      uiOutput("pca_samples_check"),
       # Plot title
-      textInput("pca_title", label = h5("Plot title"), value = "Principal Component Analysis"),
+      textInput("pca_title", label = h6("Plot title"), value = "Principal Component Analysis"),
       # Sliders for title, x-axis and y-axis text sizes.
       sliderInput("pca_size_title",
-                  h5("Title font size:"),
+                  h6("Title font size:"),
                   min = 12, max = 60,
                   value = 12, step=2),
       sliderInput("pca_size_xlab",
-                  h5("x-axis font size:"),
+                  h6("x-axis font size:"),
                   min = 10, max = 40,
                   value = 10, step=2),
       sliderInput("pca_size_ylab",
-                  h5("y-axis font size:"),
+                  h6("y-axis font size:"),
                   min = 10, max = 40,
                   value = 10, step=2),
       # size of point labels
-      numericInput("label_size", label = h5("Point label size:"), value = 2, min=0.5, max=20, step=0.5)
+      numericInput("label_size", label = h6("Point label size:"), 
+                   value = 3, 
+                   min=0.5, 
+                   max=20, 
+                   step=0.5)
     )
     ,
     # Panel for the PCA plot itself + an additional tab to select the samples
@@ -98,7 +103,7 @@ server <- function(input, output, session) {
       
       output$pca_columns1 <- renderUI({
         numericInput("pca_col1", 
-                     label = h5("Principal component on x-axis:"), 
+                     label = h6("Principal component on x-axis:"), 
                      value = 1,
                      min=1, 
                      max=10, 
@@ -107,7 +112,7 @@ server <- function(input, output, session) {
       
       output$pca_columns2 <- renderUI({
         numericInput("pca_col2", 
-                     label = h5("Principal component on y-axis:"), 
+                     label = h6("Principal component on y-axis:"), 
                      value = 2,
                      min=1,
                      max=10,
@@ -150,7 +155,7 @@ server <- function(input, output, session) {
           ),
           tabPanel("Selected samples", 
                    checkboxGroupInput("sampleschecks", 
-                                      label=h5("Samples selected: unselect and refresh to update the plot"), 
+                                      label=h6("Samples selected: unselect and refresh to update the plot"), 
                                       choices=colnames(input_pca()), 
                                       selected=colnames(input_pca()))
           )
@@ -158,18 +163,26 @@ server <- function(input, output, session) {
       })
       
     }
+    # review here: warning
     if(!is.null(input$sample_table)){
-      if(unique(input_sampletable()[,1] %in% as.character(colnames(input_pca()))) == TRUE){
+      if(any(input_sampletable()[,1] %in% as.character(colnames(input_pca())))){
         output$sample_grouping_factor <- renderUI({
-          selectInput("group_color", label = h5("Select column by which points will be colored"),
-                      choices =  colnames(input_sampletable())[-1], 
-                      selected = 1)
-          })
-        output$sample_point_labels <- renderUI({
-          selectInput("point_labels", label = h5("Select column by which points will be named"),
-                      choices =  colnames(input_sampletable()), 
+          selectInput("group_color", label = h6("Colour points by:"),
+                      choices =  c("no_colour_by", colnames(input_sampletable())[-1]), 
                       selected = 1)
         })
+        output$sample_point_labels <- renderUI({
+          selectInput("point_labels", label = h6("Name points by:"),
+                      choices =  c("no_label_by", colnames(input_sampletable())), 
+                      selected = 1)
+        })
+        
+        output$sample_point_shapes <- renderUI({
+          selectInput("point_shapes", label = h6("Shape points by:"),
+                      choices =  c("no_shape_by", colnames(input_sampletable())), 
+                      selected = 1)
+        })
+        
       }else{
         print("Please review the sample table file: the first column must correspond to the first row of the expression table!")
       }
@@ -191,10 +204,7 @@ server <- function(input, output, session) {
     pca_grouping <- input$sampleschecks
     pca_sample_names <- input$sampleschecks
     pca3 <- pca2[,..pca_samples_select]
-    # if there is a sample table file, choose grouping by color and labels
-    if (!is.null(input$file_pca)){
-      pca_grouping_colors <- input_sampletable()[input_sampletable()[,1] %in% input$sampleschecks, ]
-    }
+    
     pca_sample_names1 <- pca_sample_names[pca_samples_select]
     colnames(pca3) <- pca_sample_names1
     
@@ -203,38 +213,69 @@ server <- function(input, output, session) {
     # Retrieve the percentages of variation for each component
     percentVar <- pca$sdev^2 / sum( pca$sdev^2 )
     
-    
-    # Create data frame with principal components (elected or default), sample and group information; Create fist plot layer with or without "groups"
+    # Create data frame with principal components (elected or default) and default sample names.
     df_pca0 <- data.frame(PC1=pca$x[,input$pca_col1], 
                           PC2=pca$x[,input$pca_col2], 
                           sample=colnames(pca3))
     
-    if(!is.null(input$file_pca)){
+    # if there is a sample table file, join with expression file and then choose:
+    # 1. grouping by color 
+    # 2. labels 
+    # 3. point shape  
+    if(!is.null(input$sample_table)){
+      # extract selected samples from sample table
+      pca_grouping_colors <- input_sampletable()[input_sampletable()[,1] %in% input$sampleschecks, ]
+      # merge data frame with sample table
       df_pca <- merge(df_pca0, pca_grouping_colors, by.x="sample", by.y=1, all=T)
-      colnames(df_pca)[grep(paste0("^",input$group_color,"$"), colnames(df_pca))] <- "groups"
-      colnames(df_pca)[grep(paste0("^",input$sample_point_labels,"$"), colnames(df_pca))] <- "names"
-      p_pca <- ggplot(data=df_pca, aes(x=PC1, y=PC2, label=names, color=groups))
-    }else{
-      df_pcs <- df_pca0
-      p_pca <- ggplot(data=df_pca, aes(x=PC1, y=PC2, label=sample))
       
+      # if no point label is specified, take sample names are labels
+      if(input$point_labels != "no_label_by"){
+        # remove "names" column to replace it by user chosen column for labels
+        df_pca <- df_pca[,-grep("^sample$", colnames(df_pca))]
+        colnames(df_pca)[grep(paste0("^",input$point_labels,"$"), colnames(df_pca))] <- "name_points"
+      }else{
+        colnames(df_pca)[grep("^sample$", colnames(df_pca))] <- "name_points"
+      }
+      # depending on what parameters where set, build ggplot base layer
+      if(input$point_shapes == "no_shape_by" & input$group_color == "no_colour_by"){
+        p_pca <- ggplot(data=df_pca, aes_string(x="PC1", y="PC2", label="name_points"))
+      }else if(input$point_shapes != "no_shape_by" & input$group_color == "no_colour_by"){
+        p_pca <- ggplot(data=df_pca, aes_string(x="PC1", y="PC2", label="name_points", shape=input$point_shapes))
+      }else if(input$point_shapes == "no_shape_by" & input$group_color != "no_colour_by"){
+        p_pca <- ggplot(data=df_pca, aes_string(x="PC1", y="PC2", label="name_points", color=input$group_color))
+      }else if(input$point_shapes != "no_shape_by" & input$group_color != "no_colour_by"){
+        p_pca <- ggplot(data=df_pca, aes_string(x="PC1", y="PC2", label="name_points", color=input$group_color, shape=input$point_shapes))
+      }
+    }else{ # if there is no sample table at all
+      df_pca <- df_pca0
+      p_pca <- ggplot(data=df_pca, aes(x=PC1, y=PC2, label=sample))
     }
     # set x limits to allow space for the labels (pending letting users modify it)
     xlimits <- c(min(df_pca$PC1)*1.2, max(df_pca$PC1)*2.5)
     
-    # plot
-    p_pca + geom_point(size=3) +
+    # plot PCA
+    p_pca1 <- p_pca + geom_point(size=3) +
       theme_bw() +
       xlab(paste0("PC:",input$pca_col1," ", round(percentVar[input$pca_col1] * 100),"% variance")) +
       ylab(paste0("PC:",input$pca_col2," ", round(percentVar[input$pca_col2] * 100),"% variance")) +
       ggtitle(input$pca_title) +
-      geom_text(size=input$label_size, hjust=-0.2) +
+      geom_text(size=input$label_size, hjust=-0.1) +
       xlim(xlimits) +
       theme(plot.title = element_text(size = input$pca_size_title, face = "bold", hjust=0.5), # size of PCA plot title
             axis.title.x=element_text(size=input$pca_size_xlab), # size of x-axis label
             axis.title.y=element_text(size=input$pca_size_ylab)
             #            legend.position="none"
       )
+    # if point shapes are assigned, add more point shape options
+    if(!is.null(input$sample_table)){
+      if(input$point_shapes != "no_shape_by"){
+        p_pca1 + scale_shape_manual(values=1:length(unique(df_pca[,input$point_shapes])))
+      }else{
+        p_pca1
+      }
+    }else{
+      p_pca1
+    }
     
   })
   
@@ -263,9 +304,6 @@ server <- function(input, output, session) {
     updateActionButton(session, "doPCA",
                        label = "Refresh plot", 
                        icon("bar-chart-o"))
-    
-    
-    ## Interactive table with rhandsontable ##
     
   })
   
